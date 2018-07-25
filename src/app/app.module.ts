7ce5fa96a48ffaea0250da882070429610d42119
Trevor Stephens <tstephens@cbrintl.local>
2018-07-25 13:47:22 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
 
+//must import component class to use
 import { AppComponent } from './app.component';
+import { ServerComponent } from'./server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
-@NgModule({
+@NgModule({       //new components added to declarations array
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [      // add other modules to app; can make this module leaner/outsource some work
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
